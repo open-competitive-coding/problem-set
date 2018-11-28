8c7a5f1dc9af52ec68753188872f9457e4fb18e6
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 10:01:09 +0530

    [CI SKIP] Adding the problem

--- scripts/push-problem.sh
+++ scripts/push-problem.sh
@@ -1,5 +1,5 @@
 git config user.name "MadaraUchiha-314" && git config user.email "rohithr31@gmail.com"
-git checkout -b gh-pages
+git checkout gh-pages
 git add .
 git commit -m "[CI SKIP] Adding the problem"
 git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/open-competitive-coding/website.git origin gh-pages
