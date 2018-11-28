ca67bf7e248579371d533dcc36c6bc7ab52b9e97
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 14:39:15 +0530

    Trying

--- scripts/push-problem.sh
+++ scripts/push-problem.sh
@@ -1,6 +1,8 @@
 git config user.name "MadaraUchiha-314" && git config user.email "rohithr31@gmail.com"
 git fetch --all
-git add .
+git stash
 git checkout gh-pages
+git stash pop
+git add .
 git commit -m "[CI SKIP] Deploying the problem"
 git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/open-competitive-coding/problem-set.git gh-pages
