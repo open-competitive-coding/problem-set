7de9b168fdee5c10c48ae49794f6dd8479f58708
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 14:20:04 +0530

    Adding circleci

--- scripts/push-problem.sh
+++ scripts/push-problem.sh
@@ -1,7 +1,5 @@
 git config --global user.name "MadaraUchiha-314" && git config --global user.email "rohithr31@gmail.com"
-git stash
-git checkout gh-pages
-git stash pop
 git add .
+git checkout gh-pages
 git commit -m "[CI SKIP] Deploying the problem"
 git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/open-competitive-coding/problem-set.git gh-pages
