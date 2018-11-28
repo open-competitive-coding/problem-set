650b4b884ae636066381a437b39ec6d44c9cb5ac
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 16:57:32 +0530

    Trying the docs/ way

--- scripts/accept-problem.sh
+++ scripts/accept-problem.sh
@@ -1,16 +1,25 @@
 COMPRESSED_FOLDER_NAME="compressed"
 TAR_GZ=".tar.gz"
+DOCS="docs/"
 
 PROBLEM_ID="1-A" # Will be of the format <contest-name>-<problem-name>
 CONTEST_NAME=$(echo $PROBLEM_ID | awk -F - '{print $1}')
 PROBLEM_NAME=$(echo $PROBLEM_ID | awk -F - '{print $2}')
 
+mkdir -p $DOCS
+
 # We will create the folder compressed if it does not already exists
-mkdir -p $COMPRESSED_FOLDER_NAME
+mkdir -p $DOCS$COMPRESSED_FOLDER_NAME
 
 # We will compress the entire folder for that particular probblem
 PROBLEM_FOLDER="$CONTEST_NAME/$PROBLEM_NAME"
 
-mkdir -p $COMPRESSED_FOLDER_NAME/$CONTEST_NAME
+mkdir -p $DOCS$COMPRESSED_FOLDER_NAME/$CONTEST_NAME
 
-tar czf "$COMPRESSED_FOLDER_NAME/$CONTEST_NAME/$PROBLEM_NAME$TAR_GZ" "$CONTEST_NAME/$PROBLEM_NAME"
+tar czf "$DOCS$COMPRESSED_FOLDER_NAME/$CONTEST_NAME/$PROBLEM_NAME$TAR_GZ" "$CONTEST_NAME/$PROBLEM_NAME"
+
+mv $PROBLEM_FOLDER $DOCS$PROBLEM_FOLDER
+
+git config user.name "MadaraUchiha-314" && git config user.email "rohithr31@gmail.com"
+git add .
+git commit -m "[CI SKIP] Pushing the test cases"
