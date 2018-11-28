429fb017f1186bba4bc2ad4d771bdef795e1f197
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 14:46:48 +0530

    Trying

--- scripts/push-problem.sh
+++ scripts/push-problem.sh
@@ -1,8 +1,16 @@
+COMPRESSED_FOLDER_NAME="compressed"
+TAR_GZ=".tar.gz"
+
+PROBLEM_ID="1-A" # Will be of the format <contest-name>-<problem-name>
+CONTEST_NAME=$(echo $PROBLEM_ID | awk -F - '{print $1}')
+PROBLEM_NAME=$(echo $PROBLEM_ID | awk -F - '{print $2}')
+
 git config user.name "MadaraUchiha-314" && git config user.email "rohithr31@gmail.com"
 git fetch --all
 git add .
 git stash
 git checkout gh-pages
+rm "$COMPRESSED_FOLDER_NAME/$CONTEST_NAME/$PROBLEM_NAME$TAR_GZ"
 git stash pop
 git add .
 git commit -m "[CI SKIP] Deploying the problem"
