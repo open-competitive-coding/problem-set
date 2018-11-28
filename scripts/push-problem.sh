464c68e8ac1d209e01c6ae0deb56209391ee1751
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 14:21:46 +0530

    Adding circleci

--- scripts/push-problem.sh
+++ scripts/push-problem.sh
@@ -1,4 +1,5 @@
 git config --global user.name "MadaraUchiha-314" && git config --global user.email "rohithr31@gmail.com"
+git fetch --all
 git add .
 git checkout gh-pages
 git commit -m "[CI SKIP] Deploying the problem"
