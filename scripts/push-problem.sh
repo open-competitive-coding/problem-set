560e9deea774bc81db29fa11ede9116aa8824070
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 15:00:34 +0530

    Last Attempt

--- scripts/push-problem.sh
+++ scripts/push-problem.sh
@@ -8,6 +8,6 @@ PROBLEM_NAME=$(echo $PROBLEM_ID | awk -F - '{print $2}')
 git config user.name "MadaraUchiha-314" && git config user.email "rohithr31@gmail.com"
 git add .
 git commit -m "[CI SKIP] Deploying the problem"
-git pull --no-edit origin gh-pages
+git pull --rebase origin gh-pages
 git commit -m "[CI SKIP] Deploying the problem"
 git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/open-competitive-coding/problem-set.git gh-pages
