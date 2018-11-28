02f56e71fcf1755ff81ee220477d0d0f2b308e57
MadaraUchiha-314 <rohithr31@gmail.com>
2018-11-28 10:30:29 +0530

    Trying things

--- scripts/push-problem.sh
+++
@@ -1,6 +0,0 @@
-git config user.name "MadaraUchiha-314" && git config user.email "rohithr31@gmail.com"
-git checkout gh-pages
-git add .
-git commit -m "[CI SKIP] Adding the problem"
-git push --force --quiet https://${GITHUB_PERSONAL_TOKEN}@github.com/open-competitive-coding/problem-set.git 
-git checkout master
